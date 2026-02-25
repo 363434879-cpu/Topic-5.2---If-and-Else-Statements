2cdfc453b1342936b997dad2c61a179838da6def
363434879 <363434879@gotvdsb.ca>
2026-02-25 13:12:28 -0500

    Mostly Done!

--- Topic 5.2 - If and Else Statements/Program.cs
+++ Topic 5.2 - If and Else Statements/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int grade;
+            Console.WriteLine("What was your grade?");
+            int.TryParse(Console.ReadLine(), out grade);
+            if (grade >= 50)
+                Console.WriteLine("You Passed!");
+            else
+                Console.WriteLine("Better Luck Next Time!");
+            // The else statement is used to execute a block of code if the condition in the if statement is false. In this example, if the grade is less than 50, the else block will be executed, printing "Better Luck Next Time!" to the console.
+
+           
+
+
+
+
+
+
+
         }
-    }
+    }   
 }
