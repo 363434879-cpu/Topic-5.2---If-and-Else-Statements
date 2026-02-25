2e52340de966b8dac9a18f791fefb3e743cb533b
363434879 <363434879@gotvdsb.ca>
2026-02-25 12:55:00 -0500

    Add project files.

---
+++ Topic 5.2 - If and Else Statements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5._2___If_and_Else_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
